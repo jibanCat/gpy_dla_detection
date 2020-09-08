b6415587298a18bfedc81ca9e1da0c5727f05e39
jibancat <mho026@ucr.edu>
2020-09-08 09:27:40 +0800

    add an example script for printing the comparison of catalog redshifts and GP redshifts

--- examples/z_qso_correction.py
+++ examples/z_qso_correction.py
@@ -4,7 +4,7 @@ the difference in the result of Bayesian model selection.
 """
 from typing import List, Any, Tuple
 
-import time
+import time, os
 
 from .process_qso import process_qso
 
@@ -15,6 +15,8 @@ from gpy_dla_detection.bayesian_model_selection import BayesModelSelect
 from gpy_dla_detection.zqso_set_parameters import ZParameters
 from gpy_dla_detection.zqso_samples import ZSamples
 from gpy_dla_detection.zqso_gp import ZGPMAT
+
+from matplotlib import pyplot as plt
 
 
 def z_qso_correction(
@@ -54,15 +56,18 @@ def z_qso_correction(
 
     wavelengths, flux, noise_variance, pixel_mask = read_spec(filename)
 
-    z_qso_gp = ZGPMAT(
-        params,
-        z_qso_samples,
-        learned_file=learned_file_zestimation,
-    )
+    z_qso_gp = ZGPMAT(params, z_qso_samples, learned_file=learned_file_zestimation,)
 
     tic = time.time()
 
-    z_qso_gp.inference_z_qso(wavelengths, flux, noise_variance, pixel_mask, z_qso_min=z_qso - dz, z_qso_max=z_qso + dz)
+    z_qso_gp.inference_z_qso(
+        wavelengths,
+        flux,
+        noise_variance,
+        pixel_mask,
+        z_qso_min=z_qso - dz,
+        z_qso_max=z_qso + dz,
+    )
     print("Z True : {:.3g}".format(z_qso))
 
     toc = time.time()
@@ -85,3 +90,165 @@ def z_qso_correction(
     )
 
     return bayes, bayes_corrected, z_qso_gp, model_list, model_list_corrected
+
+
+def catalog_z_qso_check(
+    plate: int,
+    mjd: int,
+    fiber_id: int,
+    z_qso: float,
+    release="dr14q",
+    learned_file_zestimation: str = "data/dr12q/processed/learned_zqso_only_model_outdata_normout_dr9q_minus_concordance_norm_1176-1256.mat",
+):
+    """
+    give the spec id and check if the zQSO changed
+    """
+    filename = "spec-{:d}-{:d}-{:04}.fits".format(plate, mjd, fiber_id)
+
+    if not os.path.exists(filename):
+        read_spec.retrieve_raw_spec(plate, mjd, fiber_id, release=release)
+
+    params = ZParameters()
+    z_qso_samples = ZSamples(params)
+
+    if release == "dr12q":
+        wavelengths, flux, noise_variance, pixel_mask = read_spec.read_spec(filename)
+    elif release == "dr14q":
+        wavelengths, flux, noise_variance, pixel_mask = read_spec.read_spec_dr14q(
+            filename
+        )
+    else:
+        raise Exception("must choose between dr12q or dr14q!")
+
+    z_qso_gp = ZGPMAT(params, z_qso_samples, learned_file=learned_file_zestimation,)
+
+    tic = time.time()
+
+    z_qso_gp.inference_z_qso(wavelengths, flux, noise_variance, pixel_mask)
+    print("Z True : {:.3g}".format(z_qso))
+
+    toc = time.time()
+    print("spent {} mins; {} seconds".format((toc - tic) // 60, (toc - tic) % 60))
+
+    # some testing output plots to see which prediction make more sense
+    if not os.path.isdir("z_qso_check_images/"):
+        os.mkdir("z_qso_check_images/")
+
+    sample_z_qsos = z_qso_gp.z_qso_samples.sample_z_qsos()
+
+    # plot the full spectrum
+    fig, axs = plt.subplots(3, 1, figsize=(16, 15))
+
+    # 1. catalogue zQSO; also plot the model
+    z_qso_gp.set_data(
+        wavelengths,
+        flux,
+        noise_variance,
+        pixel_mask,
+        z_qso=z_qso,
+        normalize=True,
+        build_model=True,
+    )
+    axs[0].plot(z_qso_gp.x, z_qso_gp.y, label="catalogue zQSO")
+    axs[0].plot(z_qso_gp.x, z_qso_gp.this_mu, label="mean model")
+    axs[0].set_xlabel(r"Rest-frame Wavelength ($\AA$)")
+    axs[0].set_ylabel(r"Normalized Flux")
+    axs[0].set_ylim(-1, 6)
+    axs[0].legend()
+
+    # 2. sample likelihood plot
+    axs[1].plot(sample_z_qsos, z_qso_gp.sample_log_likelihoods, label="zQSO inference")
+    axs[1].vlines(
+        z_qso,
+        z_qso_gp.sample_log_likelihoods.min(),
+        z_qso_gp.sample_log_likelihoods.max(),
+        ls="--",
+        color="C1",
+        label="catalogue zQSO : {:.4g}".format(z_qso),
+    )
+    axs[1].vlines(
+        z_qso_gp.z_map,
+        z_qso_gp.sample_log_likelihoods.min(),
+        z_qso_gp.sample_log_likelihoods.max(),
+        ls="--",
+        color="C2",
+        label="zMAP : {:.4g}".format(z_qso_gp.z_map),
+    )
+    axs[1].set_xlabel("zQSO")
+    axs[1].set_ylabel("p(Data | zQSO, M)")
+    axs[1].legend()
+
+    # 3. z_map; also plot the model
+    z_qso_gp.set_data(
+        wavelengths,
+        flux,
+        noise_variance,
+        pixel_mask,
+        z_qso=z_qso_gp.z_map,
+        normalize=True,
+        build_model=True,
+    )
+    axs[2].plot(z_qso_gp.x, z_qso_gp.y, label="zMAP")
+    axs[2].plot(z_qso_gp.x, z_qso_gp.this_mu, label="mean model")
+    axs[2].set_xlabel(r"Rest-frame Wavelength ($\AA$)")
+    axs[2].set_ylabel(r"Normalized Flux")
+    axs[2].set_ylim(-1, 6)
+    axs[2].legend()
+
+    plt.savefig(
+        "z_qso_check_images/spec-{:d}-{:d}-{:04d}.pdf".format(plate, mjd, fiber_id),
+        format="pdf",
+        dpi=150,
+    )
+    plt.clf()
+    plt.close()
+
+    # plot the modelling range for DLA
+    # plot the full spectrum
+    fig, axs = plt.subplots(2, 1, figsize=(16, 10))
+
+    # 1. catalogue zQSO; also plot the model
+    z_qso_gp.set_data(
+        wavelengths,
+        flux,
+        noise_variance,
+        pixel_mask,
+        z_qso=z_qso,
+        normalize=True,
+        build_model=True,
+    )
+    ind = (z_qso_gp.x <= params.lya_wavelength) * (z_qso_gp.x >= params.lyman_limit)
+    axs[0].plot(z_qso_gp.x[ind], z_qso_gp.y[ind], label="catalogue zQSO : {:.4g}".format(z_qso))
+    axs[0].plot(z_qso_gp.x[ind], z_qso_gp.this_mu[ind], label="mean model")
+    axs[0].set_xlabel(r"Rest-frame Wavelength ($\AA$)")
+    axs[0].set_ylabel(r"Normalized Flux")
+    axs[0].legend()
+
+    # 2. z_map; also plot the model
+    z_qso_gp.set_data(
+        wavelengths,
+        flux,
+        noise_variance,
+        pixel_mask,
+        z_qso=z_qso_gp.z_map,
+        normalize=True,
+        build_model=True,
+    )
+    ind = (z_qso_gp.x <= params.lya_wavelength) * (z_qso_gp.x >= params.lyman_limit)
+    axs[1].plot(z_qso_gp.x[ind], z_qso_gp.y[ind], label="zMAP : {:.4g}".format(z_qso_gp.z_map))
+    axs[1].plot(z_qso_gp.x[ind], z_qso_gp.this_mu[ind], label="mean model")
+    axs[1].set_xlabel(r"Rest-frame Wavelength ($\AA$)")
+    axs[1].set_ylabel(r"Normalized Flux")
+    axs[1].legend()
+
+    plt.savefig(
+        "z_qso_check_images/spec-{:d}-{:d}-{:04d}_dla_range.pdf".format(
+            plate, mjd, fiber_id
+        ),
+        format="pdf",
+        dpi=150,
+    )
+    plt.clf()
+    plt.close()
+
+    return z_qso_gp
